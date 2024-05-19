2eeafd9402aea76011b48f26a5d30e8cb038e03b
KaMaDi <artkamadi@gmail.com>
2024-05-19 09:00:49 +0300

    Initial Commit

---
+++ AppForOficiant/AppForOficiantApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppForOficiantApp.swift
+//  AppForOficiant
+//
+//  Created by Jedi on 19.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppForOficiantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
